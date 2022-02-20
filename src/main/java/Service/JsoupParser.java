6bcc2c22cfbc2f03e023358491fc8ad9765585fb
SergeyPeretyagin <wodnyi33@yandex.ru>
2022-02-21 01:46:01 +0500

    coomit 16

--- src/main/java/Service/JsoupParser.java
+++ src/main/java/Service/JsoupParser.java
@@ -10,14 +10,14 @@ public class JsoupParser {
 
         Document doc = null;
         try {
-            doc = Jsoup.connect("https://api.coinlore.net/api/ticker/?id=90").ignoreContentType(true).
+            doc = Jsoup.connect("http://34.135.157.38:8085/tomcattest/send-json.jsp").ignoreContentType(true).
                     get();
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        String title = String.valueOf(doc.text());
-        String title = String.valueOf(doc.body()).replace("<body>","").replace("</body>","").replace("<p>","")
-                .replace("</p>","");
+        String title = String.valueOf(doc.text());
+//        String title = String.valueOf(doc.body()).replace("<body>","").replace("</body>","").replace("<p>","")
+//                .replace("</p>","");
         return title;
     }
 }
